db224dc6c6413c46fad6cc2480f001603135f736
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-02-25 18:11:31 +0530

    Add files via upload

---
+++ VolumeCylinder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class VolumeCylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the base radius: ");
+        double radius = sc.nextDouble();
+        System.out.print("Enter the height: ");
+        double height = sc.nextDouble();
+        double volume = Math.PI*radius * 2*height;
+        System.out.println("Volume of the prism: " + volume);
+        sc.close();
+    }
+}
