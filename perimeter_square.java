09ac100d999b9436676b4ae665c733ac62bced65
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-02-25 18:13:21 +0530

    Add files via upload

---
+++ perimeter_square.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class perimeter_square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side ");
+        double side = sc.nextDouble();
+        double perimeter = 4 * side;
+        System.out.println(perimeter);
+    }
+}
