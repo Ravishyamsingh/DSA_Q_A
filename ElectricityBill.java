3e9e6759dc737ff3442ab9de125e4dff1b342b05
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-03-06 21:29:19 +0530

    Add files via upload

---
+++ ElectricityBill.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter units consumed: ");
+        int units = sc.nextInt();
+        sc.close();
+
+        double bill = 0;
+        if (units <= 100) {
+            bill = units * 1.5;
+        } else if (units <= 300) {
+            bill = 100 * 1.5 + (units - 100) * 2.5;
+        } else {
+            bill = 100 * 1.5 + 200 * 2.5 + (units - 300) * 3.5;
+        }
+
+        System.out.println("Total Bill: ₹" + bill);
+    }
+}
