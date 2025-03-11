15a0339a55dbf20350dabd8f74458bab6eaa3da4
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-03-11 21:02:03 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int originalNum = num, sum = 0, digits = 0;
+
+        while (originalNum != 0) {
+            originalNum /= 10;
+            digits++;
+        }
+
+        originalNum = num;
+        while (originalNum != 0) {
+            int digit = originalNum % 10;
+            sum += Math.pow(digit, digits);
+            originalNum /= 10;
+        }
+
+        if (sum == num) {
+            System.out.println(num + " is an Armstrong number.");
+        } else {
+            System.out.println(num + " is not an Armstrong number.");
+        }
+
+        scanner.close();
+    }
+}
