fce6dbced71b90102e8cdd2e68c47797f876fdf4
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-13 19:27:35 +0530

    Update CurvedSurfaceAreaCylinder.java

--- CurvedSurfaceAreaCylinder.java
+++ CurvedSurfaceAreaCylinder.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CurvedSurfaceAreaCylinder {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //CurvedSurfaceAreaCylinder
         System.out.print("Enter the base radius: ");
         double radius = sc.nextDouble();
         System.out.print("Enter the base height: ");
