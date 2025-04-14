1e88043702399d3aaa55bb4f2bc84dcacd9c6db1
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-14 22:43:23 +0530

    Update Greeting_name.java

--- Greeting_name.java
+++ Greeting_name.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Greeting_name {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        
         System.out.print("Enter name:");
         String name = in.nextLine();
         System.out.println(name +" Happy morning!");
