aab0eab33d28ae13b076b864bcd6a0b030dbf1bb
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-16 22:03:40 +0530

    Update Merge_TwoSortedArrays.java

--- Merge_TwoSortedArrays.java
+++ Merge_TwoSortedArrays.java
@@ -5,6 +5,7 @@ public class Merge_TwoSortedArrays {
         // Declare two sorted integer arrays, array1 and array2
         // array1 has 'm' elements but is large enough to accommodate 'm+n' elements
         // array2 has 'n' elements
+        
 
         // Declaration and instantiation of array1
         int array1[] = new int[8];
