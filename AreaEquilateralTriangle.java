404a86af3e4053f0e4338a2d1ea09aa6428afc9f
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-18 20:48:48 +0530

    Update AreaEquilateralTriangle.java

--- AreaEquilateralTriangle.java
+++ AreaEquilateralTriangle.java
@@ -6,6 +6,7 @@ public class AreaEquilateralTriangle {
         double side,Area;
         System.out.println("Enter the side: ");
         side=in.nextDouble();
+        
         //Formula calculation
         Area=(Math.sqrt(3)/4)*Math.pow(side, 2);    //√3/4 × (side)²
         System.out.println(Area);
