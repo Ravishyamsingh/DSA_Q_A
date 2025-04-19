71696677a0526192890a413592398c300153c092
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-19 21:29:36 +0530

    Update TotalSurfaceAreaCube.java

--- TotalSurfaceAreaCube.java
+++ TotalSurfaceAreaCube.java
@@ -7,6 +7,7 @@ public class TotalSurfaceAreaCube {
         double radius = sc.nextDouble();
         System.out.print("Enter the base height: ");
         double height = sc.nextDouble();
+        
         double volume =2*Math.PI*radius*(radius+height);
         System.out.println("Volume of the prism: " + volume);
     }
