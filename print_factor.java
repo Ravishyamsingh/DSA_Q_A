07951acd1e21a5d14e71e995203a9d2ad7c94e8a
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-04-22 23:52:51 +0530

    Update print_factor.java

--- print_factor.java
+++ print_factor.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class print_factor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
         sc.close();
