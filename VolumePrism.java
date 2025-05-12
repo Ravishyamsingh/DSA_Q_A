e289607d93d4e4e7654407983043e6fe787d4761
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-12 21:07:58 +0530

    Update VolumePrism.java

--- VolumePrism.java
+++ VolumePrism.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class VolumePrism {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the base area: ");
+        
         double baseArea = sc.nextDouble();
         System.out.print("Enter the height: ");
+        
         double height = sc.nextDouble();
         double volume = baseArea * height;
+        
         System.out.println("Volume of the prism: " + volume);
         sc.close();
     }
