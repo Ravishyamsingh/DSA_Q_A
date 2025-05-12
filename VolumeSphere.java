8502b1ca8d3383e846a77b38d9a9aa78728f04dc
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-12 21:09:09 +0530

    Update VolumeSphere.java

--- VolumeSphere.java
+++ VolumeSphere.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class VolumeSphere {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the base radius: ");
+        
         double radius = sc.nextDouble();
         double volume = (4*Math.PI*radius * radius*radius)/3;
+        
         System.out.println("Volume of the prism: " + volume);
     }
 }
