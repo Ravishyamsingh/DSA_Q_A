f934ad2372dd0be4e82b0652fb6d9208388d5fda
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-14 21:23:38 +0530

    Update areaOfTriangle.java

--- areaOfTriangle.java
+++ areaOfTriangle.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class areaOfTriangle {
     public static void main(String [] args){
+        
         Scanner in = new Scanner(System.in);
         double base,height, AreaTriangle;
+        
         System.out.println("Enter the base: ");
         base=in.nextDouble();
+        
         System.out.println("Enter the Height: ");
         height=in.nextDouble();
+        
         AreaTriangle=(0.5*base*height);  //Area of Triangle=1/2(base*height)
                                          //Area Of Isosceles Triangle
                                          //Area Of Parallelogram
