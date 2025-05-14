b06457fed59bca33e95ff8589705a7d3f5030b56
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-14 21:22:16 +0530

    Update Volume_cone.java

--- Volume_cone.java
+++ Volume_cone.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class Volume_cone {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the radius: ");
+        
         double radius = sc.nextDouble();
         System.out.println("Enter the height: ");
+        
         double height = sc.nextDouble();
         double perimeter = (Math.PI*radius*radius*height)/3;
+        
         System.out.println(perimeter);
     }
 }
+
