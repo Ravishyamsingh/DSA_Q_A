79fabf62ccab4ed0ebf6985c28ae3a2076318ac2
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-15 21:06:15 +0530

    Update Add_subtract_digit.java

--- Add_subtract_digit.java
+++ Add_subtract_digit.java
@@ -19,3 +19,29 @@ public class Add_subtract_digit {
         System.out.println("Result: " + (product - sum));
     }
 }
+
+
+
+
+
+import java.util.Scanner;
+
+public class Add_subtract_digit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+        
+
+        int product = 1, sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        System.out.println("Result: " + (product - sum));
+    }
+}
