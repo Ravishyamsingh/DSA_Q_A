59ed464a7a7b021ea238adc579e9d4305c8f7666
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-15 21:09:16 +0530

    Update Arithmatic_operation.java

--- Arithmatic_operation.java
+++ Arithmatic_operation.java
@@ -34,3 +34,45 @@ public class Arithmatic_operation {
     }
 
 }
+
+
+
+
+
+
+import java.util.Scanner;
+
+
+public class Arithmatic_operation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        //Simple calculation
+        System.out.println("Enter operator");
+        //Simple calculation
+        char operator =in.next().charAt(0);
+        if (operator=='+'){
+            System.out.println(num1+num2);
+        }
+        else if(operator=='-'){
+            System.out.println(num1-num2);
+        }
+        else if (operator=='*'){
+            System.out.println(num1*num2);
+
+        } else if (operator== '/'){
+            if(num2 !=0){
+                System.out.println(num1/num2);
+            }else{
+                System.out.println("Zero division error");
+            }
+        }else{
+            System.out.println("Please enter correct input: ");
+        }
+
+
+
+    }
+
+}
