ae0dbb741df2b5b8613b5552d94f9bab85f35951
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-15 21:07:28 +0530

    Update Add_two_numm.java

--- Add_two_numm.java
+++ Add_two_numm.java
@@ -13,3 +13,23 @@ public class Add_two_numm {
         System.out.println("Sum: " + (a + b));
     }
 }
+
+
+
+
+
+import java.util.Scanner;
+
+public class Add_two_numm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        
+        sc.close();
+
+        System.out.println("Sum: " + (a + b));
+    }
+}
