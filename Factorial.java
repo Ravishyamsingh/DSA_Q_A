95b350110d22ea2508f150fdfd237d0297959628
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-18 15:07:02 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        
         int num = sc.nextInt();
         sc.close();
 
