38f35e037c8a509aef784e4a74d7bb8e236e302f
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-07-11 22:29:27 +0530

    Update areaOfTriangle.java

--- areaOfTriangle.java
+++ areaOfTriangle.java
@@ -4,7 +4,10 @@ public class areaOfTriangle {
     public static void main(String [] args){
         
         Scanner in = new Scanner(System.in);
+        
         double base,height, AreaTriangle;
+
+
         
         System.out.println("Enter the base: ");
 
