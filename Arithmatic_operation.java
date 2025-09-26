7b16432969e2c3e77064c96226475258d6d0f723
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-09-26 21:04:03 +0530

    Remove duplicate code in Arithmatic_operation.java

--- Arithmatic_operation.java
+++ Arithmatic_operation.java
@@ -1,3 +1,8 @@
+
+
+
+
+
 import java.util.Scanner;
 public class Arithmatic_operation {
     public static void main(String[] args) {
@@ -74,4 +79,5 @@ public class Arithmatic_operation {
     }
 
 }
+
 
