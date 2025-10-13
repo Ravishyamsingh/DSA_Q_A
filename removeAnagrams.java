79fe2b920a226c76e9b38bf0930e454e6949c4b3
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-10-13 21:23:24 +0530

    Create removeAnagrams.java

---
+++ removeAnagrams.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> result = new ArrayList<>();
+        result.add(words[0]);
+        
+        for (int i = 1; i < words.length; i++) {
+            if (!isAnagram(words[i], result.get(result.size() - 1))) {
+                result.add(words[i]);
+            }
+        }
+        return result;
+    }
+
+    private boolean isAnagram(String s1, String s2) {
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b);
+    }
+}
