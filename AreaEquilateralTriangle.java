edf7e1c02c2c80b0930dad1141bc8431d80a0263
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-10-31 14:46:45 +0530

    Remove duplicate code in AreaEquilateralTriangle.java

--- AreaEquilateralTriangle.java
+++ AreaEquilateralTriangle.java
@@ -1,19 +1,3 @@
-import java.util.Scanner;
-
-public class AreaEquilateralTriangle {
-    public static void main(String[] args) {
-        Scanner in= new Scanner(System.in);
-        double side,Area;
-        System.out.println("Enter the side: ");
-        
-        side=in.nextDouble();
-        
-        //Formula calculation
-        Area=(Math.sqrt(3)/4)*Math.pow(side, 2);    //√3/4 × (side)²
-        System.out.println(Area);
-
-    }
-}
 
 
 
@@ -35,6 +19,28 @@ public class AreaEquilateralTriangle {
 
     }
 }
+
+
+
+
+
+import java.util.Scanner;
+
+public class AreaEquilateralTriangle {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        double side,Area;
+        System.out.println("Enter the side: ");
+        
+        side=in.nextDouble();
+        
+        //Formula calculation
+        Area=(Math.sqrt(3)/4)*Math.pow(side, 2);    //√3/4 × (side)²
+        System.out.println(Area);
+
+    }
+}
+
 
 
 
