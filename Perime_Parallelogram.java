144318b9769cc049f46731c07c82376fad0d7947
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-11-19 21:40:37 +0530

    Refactor Perime_Parallelogram.java formatting

--- Perime_Parallelogram.java
+++ Perime_Parallelogram.java
@@ -1,6 +1,9 @@
 
 
 
+
+
+
 import java.util.Scanner;
 public class Perime_Parallelogram {
     public static void main(String[] args) {
@@ -14,4 +17,5 @@ public class Perime_Parallelogram {
         System.out.println(perimeter);
     }
 }
+
 
