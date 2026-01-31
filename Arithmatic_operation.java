b6e7895a94d9f7ec3ab8fe9b135d6abd11ac86ef
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-01-31 21:43:36 +0530

    Update Arithmatic_operation.java

--- Arithmatic_operation.java
+++ Arithmatic_operation.java
@@ -1,82 +1,35 @@
 import java.util.Scanner;
-public class Arithmatic_operation {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
-        //Simple calculation
-        System.out.println("Enter operator");
-        //Simple calculation
-        char operator =in.next().charAt(0);
-        if (operator=='+'){
-            System.out.println(num1+num2);
-        }
-        else if(operator=='-'){
-            System.out.println(num1-num2);
-        }
-        else if (operator=='*'){
-            System.out.println(num1*num2);
-
-        } else if (operator== '/'){
-            if(num2 !=0){
-                System.out.println(num1/num2);
-            }else{
-                System.out.println("Zero division error");
-            }
-        }else{
-            System.out.println("Please enter correct input: ");
-        }
-
-
-
-    }
-
-}
-
-
-
-
-
-
-import java.util.Scanner;
-
 
 public class Arithmatic_operation {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
-        //Simple calculation
-        System.out.println("Enter operator");
-        //Simple calculation
-        char operator =in.next().charAt(0);
-        if (operator=='+'){
-            System.out.println(num1+num2);
-        }
-        else if(operator=='-'){
-            System.out.println(num1-num2);
-        }
-        else if (operator=='*'){
-            System.out.println(num1*num2);
 
-        } else if (operator== '/'){
-            if(num2 !=0){
-                System.out.println(num1/num2);
-            }else{
+        System.out.print("Enter first number: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = in.nextInt();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = in.next().charAt(0);
+
+        // Simple calculation
+        if (operator == '+') {
+            System.out.println(num1 + num2);
+        } else if (operator == '-') {
+            System.out.println(num1 - num2);
+        } else if (operator == '*') {
+            System.out.println(num1 * num2);
+        } else if (operator == '/') {
+            if (num2 != 0) {
+                System.out.println(num1 / num2);
+            } else {
                 System.out.println("Zero division error");
             }
-        }else{
-            System.out.println("Please enter correct input: ");
+        } else {
+            System.out.println("Please enter correct input");
         }
 
-
-
+        in.close(); // close scanner
     }
-
 }
-
-
-
-
-
-
