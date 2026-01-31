59942c92bdc2257040f924496a162d86504bf55a
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-01-31 21:44:26 +0530

    Update ArmstrongNumber.java

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,31 +1,35 @@
-//Simple calculation
+// Simple calculation
 import java.util.Scanner;
+
 public class ArmstrongNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //Simple Calculation
-        
+
         System.out.print("Enter a number: ");
-        
         int num = scanner.nextInt();
+
         int originalNum = num, sum = 0, digits = 0;
-        
+
+        // Count digits
+        if (originalNum == 0) {
+            digits = 1; // handle 0 case
+        }
 
         while (originalNum != 0) {
             originalNum /= 10;
             digits++;
         }
 
-
-
-        
-
         originalNum = num;
+
+        // Armstrong calculation
         while (originalNum != 0) {
             int digit = originalNum % 10;
             sum += Math.pow(digit, digits);
             originalNum /= 10;
         }
+
+        System.out.println("Calculated sum = " + sum);
 
         if (sum == num) {
             System.out.println(num + " is an Armstrong number.");
@@ -33,10 +37,6 @@ public class ArmstrongNumber {
             System.out.println(num + " is not an Armstrong number.");
         }
 
-        scanner.close();
+        scanner.close(); // close scanner
     }
 }
-
-
-
-
