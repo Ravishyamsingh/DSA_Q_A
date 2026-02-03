d85ac8e635571def91187d4172ac3166dea9c26b
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-02-03 21:57:29 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -9,10 +9,39 @@ public class FibonacciSeries {
         int n = 10;
         System.out.print("Fibonacci Series: ");
         for (int i = 0; i < n; i++) {
-            System.out.print(fibonacci(i) + " ");
+            System.out.print(fibonacci(i)  + "  ");
         }
     }//
 }
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        
+        int num = sc.nextInt();
+        
+        sc.close();
+
+
+
+
+
+        
+        
+
+        long fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial: " + fact);
+    }//
+}
+
+
+
 
 
 
