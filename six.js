99963ef824146bf3691b003c8fb6793cb7a962cb
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-02-06 21:08:19 +0530

    Update six.js

--- six.js
+++ six.js
@@ -1,11 +1,8 @@
 // const coding = ["js", "ruby", "java", "python", "cpp"]
-
-
 // const values = coding.forEach( (item) => {
 //     //console.log(item);
 //     return item
 // } )
-
 // console.log(values);
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -42,4 +39,5 @@ const books = [
   userBooks = books.filter( (bk) => { 
     return bk.publish >= 1995 && bk.genre === "History"
 })
+
   console.log(userBooks);
