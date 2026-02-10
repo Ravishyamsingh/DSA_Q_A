6939d6f9abdc9f45e09c979409c38503b975e02a
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-02-10 21:35:54 +0530

    Add files via upload

--- VolumePyramid.java
+++ VolumePyramid.java
@@ -1,25 +1,13 @@
-
-
-
-
-
-
 import java.util.Scanner;
 
 public class VolumePyramid {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the base base: ");
-        
         double base = sc.nextDouble();
         System.out.print("Enter the base height: ");
-        
         double height = sc.nextDouble();
         double volume =(base*height)/3 ;
-        
         System.out.println("Volume of the prism: " + volume);
     }
 }
-
-
